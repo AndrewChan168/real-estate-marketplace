87618849b4ddffcfe123f2a2c9582bb8cf2f8ef3
Andrew Chan <46029314+AndrewChan168@users.noreply.github.com>
2019-09-16 00:08:27 +0800

    Add files via upload

--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,71 +1,87 @@
-// Test if a new solution can be added for contract - SolnSquareVerifier
+const fs = require('fs');
 const SolnSquareVerifier = artifacts.require('SolnSquareVerifier');
 const Verifier = artifacts.require('Verifier');
-const fs = require('fs');
-// Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+/*const Verifier1 = artifacts.require('Verifier1');
+const Verifier2 = artifacts.require('Verifier2');*/
 
 contract(`Test Soln Square Verifier`, accounts=>{
-    const owner = accounts[0];
-    const account1 = accounts[1];
-    const account2 = accounts[2];
-    const { proof,inputs } = JSON.parse(fs.readFileSync("../zokrates/code/square/proof.json"));
-    const trueProof = proof;
-    const fakeProof = Object.assign({}, proof);
-    fakeProof.a = fakeProof.c;
+    const owner = accounts[0]
+    const account1 = accounts[1]
+
+    var proof;
+    var inputs;
 
     beforeEach(async()=>{
-        let verifier = await Verifier.new({from:owner});
-        this.contract = await SolnSquareVerifier.new(verifier.address, {from:owner});
-    });
-
-    it(`add solution to ERC721`, async()=>{
-        this.trueProofHash = await this.contract.hashSolution.call(trueProof.a, trueProof.b, trueProof.c, inputs);
-        this.fakeProofHash = await this.contract.hashSolution.call(fakeProof.a, fakeProof.b, fakeProof.c, inputs);
-        let canAddTrue = true;
-        try {
-            await this.contract.addSolution(account1, this.trueProofHash, {from:account1});
-        } catch(err){
-            console.log(err.message);
-            canAddTrue = false;
-        }
-        assert.equal(canAddTrue, true, "Cannot add solution for true proof");
-
-        let canAddFake = true;
-        try {
-            await this.contract.addSolution(account2, this.fakeProofHash, {from:account2});
-        } catch(err){
-            console.log(err.message);
-            canAddFake = false;
-        }
-        assert.equal(canAddFake, true, "Cannot add solution for fake proof");
-
-        const trueSolutionInfo = await this.contract.getSoluction.call(this.trueProofHash);
-        const fakeSolutionInfo = await this.contract.getSoluction.call(this.fakeProofHash);
-        assert.equal(trueSolutionInfo.solver, account1, "Solver saved is incorrect for true proof");
-        assert.equal(trueSolutionInfo.index.toNumber(), 1, "Token ID saved is incorrect for true proof");
-        assert.equal(fakeSolutionInfo.solver, account2, "Solver saved is incorrect for fake proof");
-        assert.equal(fakeSolutionInfo.index.toNumber(), 2, "Token ID saved is incorrect for fake proof");
+        this.verifier = await Verifier.new({from:owner});
+        /*this.verifier1 = await Verifier1.new({from:owner});
+        this.verifier2 = await Verifier2.new({from:owner});*/
+        this.contract = await SolnSquareVerifier.new(this.verifier.address, {from:owner});
     });
 
     it(`ERC721 token will be minted for contract when correct proof provded`, async()=>{
+        let proofJson = JSON.parse(fs.readFileSync("../zokrates/code/square/proof.json"));
+        proof = proofJson.proof;
+        inputs = proofJson.inputs;
+
         let canMint = true;
         try{
-            await this.contract.mintNFT(account1, trueProof.a, trueProof.b, trueProof.c, inputs, {from:owner});
-        } catch(err){
-            console.log(err.message);
+            await this.contract.mintNFT(account1, proof.a, proof.b, proof.c, inputs, {from:owner});
+        }catch(err){
             canMint = false;
+            console.log(err.message);
         }
         assert.equal(canMint, true, "cannot be minted when correct proof provded");
     });
 
-    it(`ERC721 token will not be minted for contract when fake proof provded`, async()=>{
+    it(`ERC721 token will not be minted for contract when proof is incorrect`, async()=>{
+        /*let proofJson1 = JSON.parse(fs.readFileSync("../zokrates/code/square/proof1.json"));
+        proof = proofJson1.proof;
+        inputs = proofJson1.inputs;*/
+        let proofJson = JSON.parse(fs.readFileSync("../zokrates/code/square/proof.json"));
+        proof = proofJson.proof;
+        inputs = proofJson.inputs;
+        
         let canMint = true;
         try{
-            await this.contract.mintNFT(account1, fakeProof.a, fakeProof.b, fakeProof.c, inputs, {from:owner});
-        } catch(err){
-            console.log(err.message);
+            await this.contract.mintNFT(account1, proof.c, proof.b, proof.c, inputs, {from:owner});
+        }catch(err){
             canMint = false;
+            console.log(err.message);
         }
-        assert.equal(canMint, false, "can be minted when fake proof provded");
+        assert.equal(canMint, false, "can be minted when incorrect proof provded");
     });
-})
+
+    it(`ERC721 token could mint for several different proofs`, async()=>{
+        let proofJson = JSON.parse(fs.readFileSync("../zokrates/code/square/proofNext.json"));
+        proof = proofJson.proof;
+        inputs = proofJson.inputs;
+        let canMint;
+        canMint = true;
+        try{
+            //await this.contract.setVerifier(this.verifier1.address, {from:owner});
+            await this.contract.mintNFT(account1, proof.a, proof.b, proof.c, inputs, {from:owner});
+        }catch(err){
+            canMint = false;
+            console.log(err.message);
+        }
+        assert.equal(canMint, true, "cannot be minted when correct proof-Next provded");
+    });
+
+    it(`ERC721 could mint for 5 more tokens with different proof`, async()=>{
+        var canMint;
+        var proofJson;
+        for(let i=1;i<=5;i++){
+            proofJson = JSON.parse(fs.readFileSync(`../zokrates/code/square/proof${i}.json`));
+            proof = proofJson.proof;
+            inputs = proofJson.inputs;
+            canMint = true;
+            try{
+                await this.contract.mintNFT(account1, proof.a, proof.b, proof.c, inputs, {from:owner});
+            }catch(err){
+                canMint = false;
+                console.log(`At token-${i} with error:${err.message}`); 
+            }
+            assert.equal(canMint, true, `cannot be minted for token-${i}`);
+        }
+    });
+});
