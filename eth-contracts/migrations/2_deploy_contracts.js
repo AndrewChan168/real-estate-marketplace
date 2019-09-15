bc37e28f79659e1ad96f0f786c378361d839ed5a
Andrew Chan <46029314+AndrewChan168@users.noreply.github.com>
2019-09-16 00:09:29 +0800

    Add files via upload

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,10 +1,11 @@
-// migrating the appropriate contracts
-//var SquareVerifier = artifacts.require("./SquareVerifier.sol");
-//var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
-var ERC721Token = artifacts.require("./ERC721Token.sol");
+var Verifier = artifacts.require("./Verifier.sol");
+var VerifierNext = artifacts.require("./VerifierNext.sol");
+var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ERC721Token);
-  //deployer.deploy(SquareVerifier);
-  //deployer.deploy(SolnSquareVerifier);
+  deployer.deploy(Verifier).then(()=>{
+    return deployer.deploy(SolnSquareVerifier, Verifier.address)
+  });
+  
+ deployer.deploy(VerifierNext);
 };
